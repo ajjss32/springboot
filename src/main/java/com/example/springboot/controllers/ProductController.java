685b5d42606196e75084fb8f56e1ab36c2c106bf
ana <anajuliasilverioaf@gmail.com>
2023-08-23 08:43:45 -0300

    creating post

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -1,7 +1,15 @@
 package com.example.springboot.controllers;
 
+import com.example.springboot.dtos.ProductRecordDto;
+import com.example.springboot.models.ProductModel;
 import com.example.springboot.respositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +17,10 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
-
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto,productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 }
