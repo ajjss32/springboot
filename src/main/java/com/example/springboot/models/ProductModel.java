2fa83f383ef40fe0944e6cd47836b1ae8a49d23e
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-08-23 17:13:54 -0300

    Sending the docker config

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -1,6 +1,7 @@
 package com.example.springboot.models;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -8,6 +9,11 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "TB_PRODUCTS")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "idProduct")
 public class ProductModel implements Serializable {
     private static final long serialVersionUID =1L;
 
@@ -17,27 +23,5 @@ public class ProductModel implements Serializable {
     private String name;
     private BigDecimal value;
 
-    public UUID getIdProduct() {
-        return idProduct;
-    }
 
-    public void setIdProduct(UUID idProduct) {
-        this.idProduct = idProduct;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getValue() {
-        return value;
-    }
-
-    public void setValue(BigDecimal value) {
-        this.value = value;
-    }
 }
